11d682f0c20fece10d6848d979a376ba07d82743
Roman Tarassenko <tarassenko@me.com>
2022-11-26 09:29:55 +0300

    Применил миграцию модели Picture

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+
 # Create your models here.
+class Picture(models.Model):
+    picture = models.ImageField(
+        verbose_name="Picture",
+        null=False,
+        blank=False
+    )
+    author = models.ForeignKey(
+        verbose_name="author",
+        to=get_user_model(),
+        related_name="pictures",
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE
+    )
+    description = models.CharField(
+        verbose_name="description",
+        null=False,
+        blank=False,
+        max_length=200
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    favored_by = models.ManyToManyField(
+        to=get_user_model(),
+        related_name="favorite_pictures",
+        blank=True
+    )
--- test_9/settings.py
+++ test_9/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gallery'
+
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+#my configs
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+MEDIA_URL = "/uploads/"
+
+
 
 ROOT_URLCONF = 'test_9.urls'
 
@@ -117,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
